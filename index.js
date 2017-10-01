6055dcfc546fce6527b76ee75ddcec73ffb27bf9
byronhsu <byronhsu1230@gmail.com>
2017-10-01 14:27:18 +0800

    test

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require('babel-register');
 
-const { LINEBot, LINEHandlerBuilder} = require('toolbot-core-experiment');
-const { createServer } = require('toolbot-core-experiment/express')
+const { LineBot, LineHandlerBuilder} = require('toolbot-core-experiment');
+const { createServer } = require('toolbot-core-experiment/express');
 
 const config = {
   channelSecret: '0525fb3fce50812fcef7199e427e31fe',
   accessToken: 'nny9zy8dQR14GiUD3YUG2R/1DnObcLhkt+4KeuXBGcsX+scNDtzwy77syMcFWs5csAz0v6t6eVg36KpayHQf/HWb4C8sWnVSU6aJK3XE70jS0ecUXupnkWFcL3CEJKOFHUzV5VAYJ3cUOVDuEc2sAQdB04t89/1O/w1cDnyilFU=',
 };
 
-const bot = new LINEBot({
+const bot = new LineBot({
   channelSecret: config.channelSecret,
   accessToken: config.accessToken,
 });
@@ -28,7 +28,9 @@ const handler = new LineHandlerBuilder()
 bot.onEvent(handler);
 
 const server = createServer(bot);
+
 const port = 5000 || process.env.port;
+
 server.listen(port, () => {
   console.log('server is running on 5000 port...');
 });
