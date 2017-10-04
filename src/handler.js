dc348dbbeaa50304559571f502bb83906069494d
ccw <b02901157@ntu.edu.tw>
2017-10-04 15:59:10 +0800

    add get into

--- src/handler.js
+++ src/handler.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const { LineClient } = require('messaging-api-line');
 const { LineHandlerBuilder} = require('toolbot-core-experiment');
 var utils = require('./utils');
 
@@ -8,13 +10,6 @@ var state = 0;
 
 function mission(){
   
-}
-
-async function allocate() {
-  let cardDeck = await utils.shuffle(require('./data')[people - 5]);
-  for (let i = 0; i < users.length; i++) {
-    users[i].character = cardDeck[i];
-  }
 }
 
 module.exports = new LineHandlerBuilder()
@@ -28,15 +23,24 @@ module.exports = new LineHandlerBuilder()
 .onText('-j', async context => {
   if(state === 1){ //大家開始加入
     var currentUserId = context._session.user.id;
+    var currentUserName = context._session.user.id;
     var currentClient = context._client;
 
     if(utils.isIdExist(users,currentUserId) == false){
-      users.push({id:currentUserId,client:currentClient});
-      await context.pushText(`wait for ${playerLimit - ++player} player to start!`);
-      if(player === playerLimit){
-        users.map((user)=>{currentClient[`pushText`](user.id ,'profile');});
+      users.push({id:currentUserId,name:currentUserName,client:currentClient});
+
+      await context.pushText(`wait for ${playerLimit - ++player} players to start!`);
+      
+      if (player === playerLimit) { // game 喔喔
         state = 2;
+        utils.allocate(users);
+        // broadcast game start and all the players and characters.
+        users.map(user => {
+          LineClient.pushText(user.id, `You are ${user.character} of team ${utils.isGood(user) ? good : evil}.`);
+          LineClient.pushText(user.id, utils.getInfo(user, users));
+        })
       }
+
     }else{
       context.pushText(`You are already in the room!!`);
     }
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,44 @@
-exports.allocate = (arr)=>{
+const isGood = (user) => {
+    if (['Merlin', 'Percival', 'ServantOfArthor1', 'ServantOfArthor2', 'ServantOfArthor3', 'ServantOfArthor4'].includes(user.character)) {
+        return true;
+    }
+    return false;
+}
 
+exports.isGood = isGood;
+exports.allocate = (users)=>{
+    var data = [['Merlin', 'Percival', 'ServantOfArthor1', 'Morcana', 'Assassin'],
+    ['Merlin', 'Percival', 'ServantOfArthor1', 'ServantOfArthor2', 'Morcana', 'Assassin'],
+    ['Merlin', 'Percival', 'ServantOfArthor1', 'ServantOfArthor2', 'Morcana', 'Assassin', 'Oberon'],
+    ['Merlin', 'Percival', 'ServantOfArthor1', 'ServantOfArthor2', 'ServantOfArthor3', 'Morcana', 'Assassin', 'MinionOfMordred'],
+    ['Merlin', 'Percival', 'ServantOfArthor1', 'ServantOfArthor2', 'ServantOfArthor3', 'ServantOfArthor4', 'Morcana', 'Assassin', 'Mordred'],
+    ['Merlin', 'Percival', 'ServantOfArthor1', 'ServantOfArthor2', 'ServantOfArthor3', 'ServantOfArthor4', 'Morcana', 'Assassin', 'Oberon', 'Mordred']];
+    var cardDeck = data[users.length - 5].sort((a, b) => 0.5 - Math.random());
+    users.map((u, i) => { u.character = cardDeck[i]; });
+}
+exports.getInfo = (user, users) => {
+    let str = '';
+    if (user.character === 'Merlin') {
+        str += 'These are the bad guys:';
+        users.map(u => {
+            if (!isGood(u) && u.character !== 'Mordred') {
+                str += `\n${u.name}`;
+            }
+        });
+    } else if (user.character === 'Percival') {
+        const temp = users.filter(u => u.character === 'Merlin' || u.character === 'Morcana');
+        str += `Either ${temp[0].name} or ${temp[1].name} is Merlin.`;
+    } else if (!isGood(user) && user.character !== 'Oberon') {
+        str += 'These are the bad guys:';
+        users.map(u => {
+            if (!isGood(u) && u.character !== 'Oberon') {
+                str += `\n${u.name}`
+            }
+        });
+    } else {
+        str = 'You know nothing.'
+    }
+    return str;
 }
 exports.isIdExist = (arr,id)=>{
     for(let i=0;i<arr.length;i++){
@@ -7,9 +46,3 @@ exports.isIdExist = (arr,id)=>{
     }
     return false;
 }
-
-exports.shuffle = (arr) => {
-    arr.sort((a, b) => 0.5 - Math.random());
-    return arr;
-  };
-  
